30a617fd1368049e3ad6edaaefa18c7318f3363c
Andrio-Codes <djsi55555@gmail.com>
2022-08-10 20:54:50 +0530

    added 3a download code and separated upload and download files

--- sem5/EJ/3a upload and download/upload/FileUploadServlet.java
+++ sem5/EJ/3a upload and download/upload/FileUploadServlet.java
@@ -10,8 +10,8 @@ public class FileUploadServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        // here "/home/andrio/Uploads" is the path where to upload/save the file 
-        // and 10 is the max size of file in mB and following 1024's are multiplied to convert into kB and then Bytes
+        // here "/home/andrio/Uploads" is the path to where upload/save the file 
+        // and 10 is the max size of file in mB and following 1024's are multiplied to convert into kB and then Bytes respectively
         MultipartRequest m = new MultipartRequest(request, "/home/andrio/Uploads", 10*1024*1024);
         out.println("File successfully uploaded!");
     }
