9057c1c20170dbf5d2b9380924bdcdc040573b13
Andrio-Codes <djsi55555@gmail.com>
2022-08-16 12:42:24 +0530

    added 2b

---
+++ sem5/EJ/2b cookies/DemoCookieServlet.java
@@ -0,0 +1,24 @@
+package p1;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class DemoCookieServlet extends HttpServlet {
+    static int i = 1;
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String k = String.valueOf(i);
+        Cookie c = new Cookie("Visit", k);
+        response.addCookie(c);
+        int j = Integer.parseInt(c.getValue());
+        if(j==1) {
+            out.println("Welcome");
+        }
+        else {
+            out.println("You visited " + i + " times");
+        }
+        i++;        
+    }
+}
