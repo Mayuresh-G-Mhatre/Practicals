5a8af0de9fed71d7a26c7ebe33e7baac79a97c2f
Andrio-Codes <djsi55555@gmail.com>
2022-10-01 15:52:44 +0530

    added awp 2a1

---
+++ sem5/AWP/2a1 factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2a1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, number, fact;
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            fact = 1;
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nFactorial of given number is: " + fact);
+            Console.ReadLine();
+        }
+    }
+}
